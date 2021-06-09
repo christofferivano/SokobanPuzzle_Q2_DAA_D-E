f88bdc614cd6c606793ca8bd9807ab5b5140c9af
vincentyonathan <vincentyonathan24@gmail.com>
2021-06-09 12:24:20 +0700

    add Nodes, fix main, add State

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
+    
     public static Solution bfs(Map map)
     {
-        
+        long Timer = System.currentTimeMillis(); //returns current time in ms
+        int nodes = 1;
+        int redun = 0;
+        Node
+
+        return null;
     }
 }
